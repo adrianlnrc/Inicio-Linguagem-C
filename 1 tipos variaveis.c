10b9787e2e444a5dc63996fa3a02c2e750064c3e
Adrian alencar <spottyadrian@gmail.com>
2025-06-13 10:30:08 -0300

    implementacao de variaveis

---
+++ 1 tipos variaveis.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+//tipos mais usados das variaveis
+
+int numero = 1;
+float preco = 5.99;
+char letra = 'D';
+char string[10] = "Doritos";
+
+//mostra ao usuario
+
+printf("\n no estoque: %d \n preco:%f \n primeira letra: %c \n Salgadinho:%s\n", numero, preco, letra,string);
+
+
+
+}
