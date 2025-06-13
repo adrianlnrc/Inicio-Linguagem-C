c080efcff0c5a19ff0c3dcafe967d79a1af4cd9b
Adrian alencar <spottyadrian@gmail.com>
2025-06-13 10:36:41 -0300

    implementacao de for e while

---
+++ 3 for e while.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+//(inicializacao, inicia a variavel com um valor),(condicao, enquando for menor ou igual a 10),(incremento i+1)
+for(int i = 0;i <=10;i++)
+{
+    printf("%d\n",i);
+    printf("");
+}    
+
+int i = 0;
+
+
+// é utilizado quando nao se sabe a quantidade de interacoes a serem executadas
+while (i <=5)
+{
+    printf("%d\n",i);
+    i++;
+}
+
+
+
+
+
+
+
+
+
+}
